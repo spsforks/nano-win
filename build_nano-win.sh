e6259e3603d84b1eb456755eb6625acd69410fbc
LH_Mouse <lh_mouse@126.com>
2022-02-02 21:44:47 +0800

    Revert "Update build_nano-win.sh"
    This reverts commit 4f78788b7b6fda4a0c990b07cf6692e7dc90f763.

--- build_nano-win.sh
+++ build_nano-win.sh
@@ -30,7 +30,7 @@ pushd "ncurses"
   --without-ada --without-cxx-binding --disable-db-install --without-manpages  \
   --without-pthread --without-debug --enable-widec --disable-database  \
   --disable-rpath --enable-termcap --disable-home-terminfo --enable-sp-funcs  \
-  --enable-term-driver --enable-static --disable-shared --without-tests
+  --enable-term-driver --enable-static --disable-shared
 make -j"${_nproc}"
 make install
 popd
